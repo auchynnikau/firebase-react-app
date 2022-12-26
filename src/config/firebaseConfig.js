32a990d16748cb9095eb765d0283625415742f3c
auchynnikau <viachaslau_auchynnikau@epam.com>
2022-12-26 23:14:20 +0300

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -3,13 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyCgyxuYa3vpK1b87Jrv-uzm_DGTreEBtwE',
-  authDomain: 'sightseeing-guide.firebaseapp.com',
-  databaseURL: 'https://sightseeing-guide.firebaseio.com',
+  apiKey: 'API_KEY',
+  authDomain: 'DOMAIN',
+  databaseURL: 'DB_URL',
   projectId: 'sightseeing-guide',
-  storageBucket: 'sightseeing-guide.appspot.com',
-  messagingSenderId: '127557907156',
-  appId: '1:127557907156:web:a2447e40773e4ee0'
+  storageBucket: 'STORAGE_BUCKET',
+  appId: 'APP_ID'
 };
 
 firebase.initializeApp(firebaseConfig);
